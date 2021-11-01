50d2df60017aa6ff12c46ff9ad35e1fca2fd5fd3
jegyed50 <26919968+jegyed50@users.noreply.github.com>
2021-11-01 19:32:18 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,39 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 99999999999999; index++) {
+        pins.analogWritePin(AnalogPin.P0, 1023)
+        basic.pause(1000)
+        pins.analogWritePin(AnalogPin.P0, 0)
+        basic.pause(500)
+        pins.analogWritePin(AnalogPin.P0, 1023)
+        basic.pause(1000)
+        pins.analogWritePin(AnalogPin.P0, 0)
+        basic.pause(500)
+        pins.analogWritePin(AnalogPin.P0, 1023)
+        basic.pause(1000)
+        pins.analogWritePin(AnalogPin.P0, 0)
+        basic.pause(500)
+        pins.analogWritePin(AnalogPin.P0, 1023)
+        basic.pause(500)
+        pins.analogWritePin(AnalogPin.P0, 0)
+        basic.pause(60000)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index <= 10; index++) {
+        pins.analogPitch(index + 2, 500)
+        music.stopAllSounds()
+        basic.pause(50)
+    }
+    music.stopAllSounds()
+    basic.pause(500)
+    for (let index = 0; index <= 10; index++) {
+        pins.analogPitch(20 - (index + 2), 500)
+        music.stopAllSounds()
+        basic.pause(50)
+    }
+})
+pins.analogWritePin(AnalogPin.P0, 1023)
+music.setVolume(255)
+basic.forever(function () {
+	
+})
